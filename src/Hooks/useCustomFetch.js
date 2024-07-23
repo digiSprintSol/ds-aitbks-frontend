5376e3eb11457e12ee11c929c62de1613b8f5124
sathwik-digi <ssmajeti@digisprint.com>
2024-07-23 16:29:27 -0700

    feat(http): create custom hooks for axios interceptor for http request

---
+++ src/Hooks/useCustomFetch.js
@@ -0,0 +1,29 @@
+// useCustomFetch.js
+
+import { useEffect, useState } from "react";
+import { instance } from "../https";
+
+const useCustomFetch = (url, method = "get") => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await instance[method](url);
+        setData(response.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+
+export default useCustomFetch;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
+/* eslint-disable no-return-await */
 import React from "react";
+import useCustomFetch from "../Hooks/useCustomFetch";
 // import { useRootContext } from "../Hooks/useRootContext";
 
 function Home() {
+  const { data, loading, error } = useCustomFetch(
+    "https://jsonplaceholder.typicode.com/users",
+    "get"
+  );
   // const theme = useRootContext();
-  // console.log("theme", theme);
-  return <div>Home</div>;
+  // console.log("theme", { data, loading, error });
+  if (error) return <h1>Error .....</h1>;
+  if (loading) return <h1>loading .....</h1>;
+  return <div>Home ,{JSON.stringify(data, null, 2)}</div>;
 }
 
 export default Home;
