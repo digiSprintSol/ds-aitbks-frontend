3bb470f4b16515c2b87f8da28c1269a6e7bf38cb
monishka-digi <mranjan@digisprint.com>
2024-08-05 17:24:25 +0530

    feat(component): implemented font and modified president's view UI

--- src/pages/UploadAnnouncement.jsx
+++ src/pages/UploadAnnouncement.jsx
@@ -1,29 +1,44 @@
 import React from "react";
+import { Box, Grid, TextField } from "@mui/material";
 import upload2 from "./images/upload2.png";
+import Button from "../components/Button";
 import "../App.css";
 
 function UploadAnnouncement() {
   return (
-    <div className="uploadannouncementhead">
-      <h1>Make a Announcement</h1>
-      <h1>Announcement Details</h1>
-      <input type="text" placeholder="Title" className="uploadeventinput1" />
-      <br />
-      <br />
-      <input
-        type="text"
-        placeholder="Description"
-        className="uploadeventinput2"
-      />
-      <br />
-      <br />
-      <button type="button" className="uploadannouncementbuttonclass">
-        <div>
-          <span>Post </span>
-          <img src={upload2} alt="smallupload" height="15vw" width="15vw" />
-        </div>
-      </button>
-    </div>
+    <Box
+      className="uploadannouncementhead"
+      sx={{ width: "auto", display: "flex", flexDirection: "column" }}
+    >
+      <h1 style={{ fontFamily: "ProximaBold" }}>Make a Announcement</h1>
+      <h1 style={{ fontFamily: "ProximaSemiBold" }}>Announcement Details</h1>
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <TextField
+            type="text"
+            placeholder="Title"
+            className="uploadeventinput1"
+            sx={{ backgroundColor: "white" }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            type="text"
+            placeholder="Description"
+            className="uploadeventinput2"
+            sx={{ backgroundColor: "white" }}
+          />
+        </Grid>
+        <Grid item xs={12}>
+          <Button type="button" className="uploadannouncementbuttonclass">
+            <div>
+              <span>Post </span>
+              <img src={upload2} alt="smallupload" height="20vw" width="20vw" />
+            </div>
+          </Button>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
